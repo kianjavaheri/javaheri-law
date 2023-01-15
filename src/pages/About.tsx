db4d529b762d05e32a14464c2cf4db81d813a84c
Kian Javaheri <kianjavaheri911@gmail.com>
2023-01-15 08:42:46 -0700

    a

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './../styling/pages/About.css'
+import ali from './../util/imgs/ali.png'
 
 function About() {
   return (
@@ -7,24 +8,31 @@ function About() {
       <div id="about-wrapper">
         <div id="text-wrapper">
           <div id="about">
-            <h1>About Me</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, incidunt voluptatibus et ipsam voluptatem, minima ullam, odio vero doloribus quod labore at exercitationem nulla nam. Cupiditate quis reprehenderit placeat dolorem ad cum magnam accusamus praesentium autem omnis, ducimus labore ab voluptatum, nulla architecto. Officia velit suscipit voluptas ea rerum doloribus repellendus asperiores quas, fuga autem aliquam eaque provident! Praesentium explicabo maiores consequuntur eveniet, rem quae repellat quibusdam ea assumenda at totam, accusamus molestiae dicta exercitationem nobis optio, incidunt illo reprehenderit!
-              </p>
-          </div>
+            
+            <div id="about-content">
+              <div id="text-content">
+                  <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, incidunt voluptatibus et ipsam voluptatem, minima ullam, odio vero doloribus quod labore at exercitationem nulla nam. Cupiditate quis reprehenderit placeat dolorem ad cum magnam accusamus praesentium autem omnis, ducimus labore ab voluptatum, nulla architecto. Officia velit suscipit voluptas ea rerum doloribus repellendus asperiores quas, fuga autem aliquam eaque provident! Praesentium explicabo maiores consequuntur eveniet, rem quae repellat quibusdam ea assumenda at totam, accusamus molestiae dicta exercitationem nobis optio, incidunt illo reprehenderit!
+                  </p>
+              </div>
+              <div id="image-content">
+                <img src={ali} alt="Ali Headshot"></img>
+              </div>
+              </div>
 
-          <div id="memberships">
-            <h1>Memberships</h1>
-            <ul>
-              <li>State Bar of California</li>
-              <li>United States District Court, Northern District of California</li>
-              <li>American Bar Association</li>
-              <li>Alameda County Bar Association</li>
-              <li>Contra Costa County Bar Association</li>
-              <li>Real Estate Broker, California Department of Real Estate</li>
-            </ul>
+            <div id="memberships">
+              <h1>Memberships</h1>
+              <ul>
+                <li>State Bar of California</li>
+                <li>United States District Court, Northern District of California</li>
+                <li>American Bar Association</li>
+                <li>Alameda County Bar Association</li>
+                <li>Contra Costa County Bar Association</li>
+                <li>Real Estate Broker, California Department of Real Estate</li>
+              </ul>
+            </div>
           </div>
-
+          
         </div>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './../styling/pages/Home.css'
-
-import wave from './../util/svg/wave.svg'
+import supreme from './../util/imgs/supreme_court.jpg'
 
 function Home() {
   return (
     <div>
       <div id="home-wrapper">
         <div id="heading">
+          <img src={supreme} alt="Supreme Court Banner" />
+          <div id="gradient"></div>
           <h1>Law Offices of Ali J. Javaheri</h1>
         </div>
         <div id="general-info">
